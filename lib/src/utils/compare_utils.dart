f277ab9a2324e81ed8f55daf83250e30122867e1
nashol <victor.dunaev@gmail.com>
2026-07-30 05:10:27 +0500

    fix infinite recursion in CompareUtils.identical

--- lib/src/utils/compare_utils.dart
+++ lib/src/utils/compare_utils.dart
@@ -1,10 +1,13 @@
+import 'dart:core';
+import 'dart:core' as core;
+
 // ignore: avoid_classes_with_only_static_members
 abstract final class CompareUtils {
   static bool equals(Object? a, Object? b) => a == b;
 
   static bool notEquals(Object? a, Object? b) => a != b;
 
-  static bool identical(Object? a, Object? b) => identical(a, b);
+  static bool identical(Object? a, Object? b) => core.identical(a, b);
 
-  static bool notIdentical(Object? a, Object? b) => !identical(a, b);
+  static bool notIdentical(Object? a, Object? b) => !core.identical(a, b);
 }
